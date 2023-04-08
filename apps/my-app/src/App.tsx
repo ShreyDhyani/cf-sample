a08c4a6a16bb9baca1a964eb86741f252db4a71c
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-04-08 23:18:53 +0530

    Importing from packages

--- apps/my-app/src/App.tsx
+++ apps/my-app/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import { Dashboard, ActivityPage, ProfilePage } from "ui";
 
 function App() {
   const list = ["dashboard", "activity", "profile"];
@@ -19,9 +20,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<div>Home</div>} />
-          <Route path="/dashboard" element={<div>Dashboard</div>} />
+          {/* <Route path="/dashboard" element={<div>Dashboard</div>} />
           <Route path="/activity" element={<div>Activity</div>} />
-          <Route path="/profile" element={<div>Profile</div>} />
+          <Route path="/profile" element={<div>Profile</div>} /> */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/activity" element={<ActivityPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
         </Routes>
       </Router>
     </div>
