b2c2cb2ea4ff1270bffd81f519ac4e414c3c78a3
Shrey Dhyani <betalectic@Shreys-MacBook-Air.local>
2023-04-08 23:19:30 +0530

    added pakages

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,6 @@
 import * as React from "react";
 export * from "./Button";
+
+export { default as Dashboard } from "./Dashboard/index";
+export { default as ActivityPage } from "./ActivityPage/index";
+export { default as ProfilePage } from "./ProfilePage/index";
